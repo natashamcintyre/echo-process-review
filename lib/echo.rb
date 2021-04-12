55050d9a7df3c4d103ac57228e7efa1893fe2690
Natasha McIntyre <natashamc.cs@gmail.com>
2021-04-12 18:19:03 +0100

    Loop until exit typed

--- lib/echo.rb
+++ lib/echo.rb
@@ -4,15 +4,19 @@ end
 
 def introduce
   puts "Say something:"
-  listen_for_text
 end
 
 def listen_for_text
   mytext = gets.chomp
-  if mytext == "exit"
-    goodbye
-  else
+  process_text(mytext)
+end
+
+def process_text(mytext)
+  if mytext != "exit"
     echo(mytext)
+    listen_for_text
+  else
+    goodbye
   end
 end
 
@@ -21,3 +25,4 @@ def goodbye
 end
 
 introduce
+listen_for_text
--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -2,10 +2,10 @@ require 'echo'
 
 describe '#echo' do
   it "returns yyyy-mm-dd | hh:mm | You said: 'hello, world'! when input is hello, world" do
-    expect(echo("hello, world")).to eq("#{Time.now.strftime("%Y-%m-%d")} | #{Time.now.strftime("%k:%M")} | You said: 'hello, world'!")
+    expect{ echo("hello, world") }.to output("#{Time.now.strftime("%Y-%m-%d")} | #{Time.now.strftime("%k:%M")} | You said: 'hello, world'!\n").to_stdout
   end
 
   it "returns yyyy-mm-dd | hh:mm | You said: 'my input'! when input is my input" do
-    expect(echo("my input")).to eq("#{Time.now.strftime("%Y-%m-%d")} | #{Time.now.strftime("%k:%M")} | You said: 'my input'!")
+    expect{ echo("my input") }.to output("#{Time.now.strftime("%Y-%m-%d")} | #{Time.now.strftime("%k:%M")} | You said: 'my input'!\n").to_stdout
   end
 end
